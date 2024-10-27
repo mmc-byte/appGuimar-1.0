e36286d0c8c96dc8e19d91f48c14ad052dff8073
Michaelpizarro18 <mpizarrota@ucvvirtual.edu.pe>
2024-10-26 19:43:58 -0500

    Ventana Principal y Login con los estilos para niños adolescentes(Si gustan cambien de color)

--- frontend/guimar-app/src/app/main/main.page.ts
+++ frontend/guimar-app/src/app/main/main.page.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component,AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {IonicModule} from '@ionic/angular';
 import { RouterLink } from '@angular/router';
+
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-main',
@@ -11,10 +13,35 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [IonicModule, CommonModule, FormsModule,RouterLink]
 })
-export class MainPage  {
+export class MainPage implements AfterViewInit {
+  constructor(private navCtrl: NavController) {}
 
-  constructor() { }
+  ngAfterViewInit() {
+    this.initializeParticles();
+  }
 
+  private initializeParticles() {
+    const RANDOM = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1) + min);
+    const PARTICLES = document.querySelectorAll('.particle');
 
-
+    PARTICLES.forEach(P => {
+      P.setAttribute('style', `
+        --x: ${RANDOM(20, 80)};
+        --y: ${RANDOM(20, 80)};
+        --duration: ${RANDOM(6, 20)};
+        --delay: ${RANDOM(1, 10)};
+        --alpha: ${RANDOM(40, 90) / 100};
+        --origin-x: ${Math.random() > 0.5 ? RANDOM(300, 800) * -1 : RANDOM(300, 800)}%;
+        --origin-y: ${Math.random() > 0.5 ? RANDOM(300, 800) * -1 : RANDOM(300, 800)}%;
+        --size: ${RANDOM(40, 90) / 100};
+      `);
+    });
+  }
+  navigateToPage() {
+    setTimeout(() => {
+        this.navCtrl.navigateForward('/h-sinconexion'); // Ajusta la ruta
+    }, 1000); // Retraso de 1 segundo
 }
+}
+
+
